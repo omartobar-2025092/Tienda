60bdfd8c28c5cd73d759df3ae6380782bd2b0cea
omartobar-2025092 <otobar-2025092@kinal.edu.gt>
2026-04-15 22:24:51 -0600

    fix: Se soluciono un error por parte de Usuarios repository

--- backendTienda/src/main/java/com/omartobar/backendTienda/Repository/UsuariosRepository.java
+++ backendTienda/src/main/java/com/omartobar/backendTienda/Repository/UsuariosRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuariosRepository extends JpaRepository<Usuarios, Integer> {
-
+    Usuarios findByUsername(String username);
 }
