bd05d6e7c02927c89525d95cb75bd1d0d25d0997
omartobar-2025092 <otobar-2025092@kinal.edu.gt>
2026-04-15 23:00:28 -0600

    fix: Se arreglo la url de cada controller

--- backendTienda/src/main/java/com/omartobar/backendTienda/Controller/ClientesController.java
+++ backendTienda/src/main/java/com/omartobar/backendTienda/Controller/ClientesController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/clientes")
+@RequestMapping("/clientes")
 
 public class ClientesController {
 
--- backendTienda/src/main/java/com/omartobar/backendTienda/Controller/ProductosController.java
+++ backendTienda/src/main/java/com/omartobar/backendTienda/Controller/ProductosController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/productos")
+@RequestMapping("/productos")
 
 public class ProductosController {
 
